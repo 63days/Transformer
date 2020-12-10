5bd240cc0c2decb931e8f592fb3e76524e4f15fe
63days <tretre069@naver.com>
2020-12-10 14:52:13 +0900

    add pad_mask, subsequent_mask

---
+++ masking.py
@@ -0,0 +1,12 @@
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def get_pad_mask(seq, pad_idx):
+    return (seq == pad_idx)
+
+def get_subsequent_mask(seq):
+    len_seq = seq.size(-1)
+    mask = torch.triu(torch.ones((len_seq, len_seq), device=device), diagonal=1)
+
+    return mask
